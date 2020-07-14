4f226c7f0374b007cf90a3f46117c7b08bdbc233
cakasuma <amammustofa@gmail.com>
2020-07-14 15:53:09 +0800

    Updates

---
+++ precache-manifest.63fff2dfa71549463f9bcc80568bc96a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69c574dd1d1d379e38a6ecc00ea2183",
+    "url": "/react_redux_template_auth/index.html"
+  },
+  {
+    "revision": "45db71346cb2fe1fa48f",
+    "url": "/react_redux_template_auth/static/css/main.7cb117e3.chunk.css"
+  },
+  {
+    "revision": "c3fa1822278ec5d034a2",
+    "url": "/react_redux_template_auth/static/js/2.be13f3da.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/react_redux_template_auth/static/js/2.be13f3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45db71346cb2fe1fa48f",
+    "url": "/react_redux_template_auth/static/js/main.09ebad12.chunk.js"
+  },
+  {
+    "revision": "3cd75b8d6dd7b42ba6e4",
+    "url": "/react_redux_template_auth/static/js/runtime-main.fffc57f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_redux_template_auth/static/media/logo.5d5d9eef.svg"
+  }
+]);
